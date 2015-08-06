edd00b4c4c33b1cfe60230f66e6f1a36a69cb4d7
jvillmo <jvillmo@gmail.com>
2015-08-06 10:01:14 -0600

    * Inicial

---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,23 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/src/windows/StatusBarProxy.js",
+        "id": "org.apache.cordova.statusbar.StatusBarProxy",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.statusbar": "0.1.10"
+}
+// BOTTOM OF METADATA
+});
